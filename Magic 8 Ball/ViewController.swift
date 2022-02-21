8943641bfff3781ade50acca327d485afd9cabc7
torinunna <yujin9588@gmail.com>
2022-02-22 00:26:41 +0900

    randomise the imageView

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
    
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+        
+        imageView.image = ballArray[Int.random(in: 0...4)]
     }
     
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    
 
 
 }
